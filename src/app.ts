05bd9080b7637aefb172fd51292e73cb99574586
Oliver Stenkilde <olm@tricom.dk>
2016-12-16 11:17:16 +0100

    Angular Typescript progress

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,32 @@
 import * as angular from 'angular';
-import * as router from 'angular-ui-router';
+import 'angular-ui-router';
 
-export let app = angular.IModule = angular.module('app', ['ui.router']);
+export let app = angular.module('app', ['ui.router']);
 
-app.config((stateProvider: router.IStateProvider) => {
+app.config([
+    function ($stateProvider: angular.ui.IStateProvider, $urlRouterProvider: angular.ui.IUrlRouterProvider) {
+        let pageNotFoundState = {
+            name: 'pagenotfound',
+            url: '/pagenotfound',
+            template: '<h1>404</h1>'
+            // templateUrl: 'src/views/pageNotFound/PageNotFound.html',
+            // controller: 'pageNotFoundController',
+            // controllerAs: 'vm',
+            // bindToController: true,
+        };
 
-});
+        let homeState = {
+            name: 'home',
+            url: '/',
+            templateUrl: 'src/containers/Home/Home.html',
+            controller: 'homeController',
+            controllerAs: 'vm'
+        };
+
+        $stateProvider.state(homeState);
+        $stateProvider.state(pageNotFoundState);
+
+        $urlRouterProvider.when('', '/');
+        $urlRouterProvider.otherwise('/pagenotfound');
+    }
+]);
---
+++ src/containers/Home/Home.js
@@ -0,0 +1,5 @@
+class HomeController {
+    static $inject = [];
+    
+    constructor() {};
+};
